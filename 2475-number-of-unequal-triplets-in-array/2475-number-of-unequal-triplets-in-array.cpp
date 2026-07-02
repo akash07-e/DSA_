48de676ad148d1b02ac22db77b9e556de445bc04
akash07-e <akash072910@gmail.com>
2026-07-02 17:29:23 +0530

    Time: 3 ms (74.58%), Space: 11.6 MB (11.51%) - LeetHub

--- 2475-number-of-unequal-triplets-in-array/2475-number-of-unequal-triplets-in-array.cpp
+++ 2475-number-of-unequal-triplets-in-array/2475-number-of-unequal-triplets-in-array.cpp
@@ -1,28 +1,40 @@
 class Solution {
 public:
     int unequalTriplets(vector<int>& nums) {
-       
-        // int i=0;
-        // int j=1;
-        // int k=nums.size()-1;
-        int count =0;
-        for(int i=0;i<=nums.size();i++){
-            for(int j=i+1;j<nums.size();j++){
-                for(int k=j+1;k<nums.size();k++){
-                     if(nums[i]!=nums[j]&&nums[i]!=nums[k]&&nums[j]!=nums[k]){
-               count++;
-              }
+        unordered_map<int,int> mp;
+
+        // Frequency count
+        for(int x : nums) {
+            mp[x]++;
+        }
+
+        vector<int> freq;
+        for(auto it : mp) {
+            freq.push_back(it.second);
+        }
+
+        int count = 0;
+        int m = freq.size();
+
+        // Choose 3 distinct value groups
+        for(int i = 0; i < m; i++) {
+            for(int j = i + 1; j < m; j++) {
+                for(int k = j + 1; k < m; k++) {
+                    count += freq[i] * freq[j] * freq[k];
                 }
             }
         }
-        // while(j<k||k>0||j<nums.size()-1){
-        //      if(nums[i]!=nums[j]&&nums[i]!=nums[k]&&nums[j]!=nums[k]){
-        //       count++;
-        //      }
-           
-        //     else k--;
+        /*count += freq[0] * freq[1] * freq[2]
 
-        // }
-       return count ;
+Substitute:
+
+count += 3 * 1 * 1
+
+So:
+
+count = 3
+*/
+
+        return count;
     }
 };
